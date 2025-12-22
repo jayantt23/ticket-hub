2f3a9e0ff1255a886767efe506743a4741304f35
jayantt23 <jayant.sharma@iiitb.ac.in>
2025-12-22 20:47:36 +0530

    Initial commit: Working multi-module build setup

---
+++ backend/notification-service/src/test/java/com/jayant/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayant.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
