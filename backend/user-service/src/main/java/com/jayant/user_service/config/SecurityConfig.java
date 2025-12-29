ac4befbd084f3b4e362b1ad404a97734d97817be
jayantt23 <jayant.sharma@iiitb.ac.in>
2025-12-29 13:54:03 +0530

    docs: add openapi swagger to all microservices

--- backend/user-service/src/main/java/com/jayant/user_service/config/SecurityConfig.java
+++ backend/user-service/src/main/java/com/jayant/user_service/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
