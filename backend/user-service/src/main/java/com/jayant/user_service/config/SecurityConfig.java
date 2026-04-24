57d045000249af50d71adc84e200df852e058e32
jayantt23 <jayant.sharma@iiitb.ac.in>
2026-04-24 19:51:27 +0530

    feat: implement full observability stack with Prometheus, Grafana, and Zipkin

--- backend/user-service/src/main/java/com/jayant/user_service/config/SecurityConfig.java
+++ backend/user-service/src/main/java/com/jayant/user_service/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/actuator/**").permitAll() // Add this line to allow Prometheus
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
